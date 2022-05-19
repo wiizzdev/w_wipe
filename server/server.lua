e7cd4fc1c0f5680412f04ce485616bc5ce05524b
wiizz <66723882+wiizzdev@users.noreply.github.com>
2022-05-19 11:23:16 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -11,12 +11,13 @@ RegisterNetEvent("wipe:wipe")
 AddEventHandler("wipe:wipe", function(idjoueur, nomjoueur)
     local xPlayer = ESX.GetPlayerFromId(idjoueur)
     DropPlayer(idjoueur, "Vous avez été wipe du serveur. Vous avez donc été déconnecté")
+    TriggerClientEvent('esx:showNotification', source, "Vous avez wipe ~b~"..nomjoueur)
     MySQL.Async.execute([[ 
         DELETE FROM users WHERE identifier = @identifier; ]], {
             ['@identifier'] = xPlayer.getIdentifier(idjoueur),
         }, function()
     end)
     --[[
-        copier-coller les lignes 14 à 18 pour pouvoir wipe d'autres tables de la bdd (ne pas oublier de remplacer users par le nom de la table)
+        copier-coller les lignes 15 à 19 pour pouvoir wipe d'autres tables de la bdd (ne pas oublier de remplacer users par le nom de la table)
     ]]--
 end)
